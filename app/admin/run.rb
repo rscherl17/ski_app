c13d1a664c39dc70c83a7b965a94717377b018ea
Raghu Betina <raghu@firstdraft.com>
2017-03-01 14:50:35 +0000

    Whitelist columns in Run for ActiveAdmin

--- app/admin/run.rb
+++ app/admin/run.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Run do
 
+ permit_params :mountain_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
