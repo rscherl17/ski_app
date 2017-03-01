076e208c41a0c2a6918dbb7fab66722bc9ea1eeb
Raghu Betina <raghu@firstdraft.com>
2017-03-01 14:50:10 +0000

    Direct association between runs and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :run
+
   belongs_to :user
 
   # Indirect associations
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,6 +1,9 @@
 class Run < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   belongs_to :mountain
 
   # Indirect associations
